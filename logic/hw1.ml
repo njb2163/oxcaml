a45a562833b043e1fd6760c6d51f66bb5901320d
Nicholas Bindela <157432778+njb2163@users.noreply.github.com>
2025-09-14 18:51:16 +0000

    added initial game state

--- logic/hw1.ml
+++ logic/hw1.ml
@@ -23,7 +23,7 @@ type player =
 id : player_id
 ; name : string option
 ; hand : card list
-; role : role
+; role : role option
 ; prev_position : int option (* What place the user came in last round, used for deck picking *)
 ; has_passed : bool (* Needed to know if we have gone a full loop *)
 ; total_points : int (* How many points they have in total *)
@@ -48,7 +48,7 @@ type phase = (* What phase of the game is it*)
 type turn_state =
 {
 whose_turn: player_id
-; starting_player: player_id option
+; starting_player: player_id option (* Who played the first card in the trick *)
 }
 
 type decision =
@@ -81,5 +81,74 @@ players : player list
 ; table : table_state
 ; phase : phase
 ; decision : decision
+}
+
+(* ---------- Initial Game Setup  ------------ *)
+
+let player1 : player = {
+id = 0
+; name = Some "John"
+; hand = []
+; role = None
+; prev_position = None
+; has_passed = false
+; total_points = 0
+}
+
+let player2 : player = {
+id = 1
+; name = Some "Paul"
+; hand = []
+; role = None
+; prev_position = None
+; has_passed = false
+; total_points = 0
+}
+
+let player3 : player = {
+id = 2
+; name = Some "Ringo"
+; hand = []
+; role = None
+; prev_position = None
+; has_passed = false
+; total_points = 0
+}
+
+let player4 : player = {
+id = 3
+; name = Some "George"
+; hand = []
+; role = None
+; prev_position = None
+; has_passed = false
+; total_points = 0
+}
+
+
+let initial_rules : rules =
+{
+clear_on_two = false
+; quad_bomb = false
+; starting_card = None
+; max_players = 4
+}
+
+let initial_state : game_state =
+let initial_table_state = 
+{
+current_requirement = None
+; last_advancer = None
+; passes_in_row = 0 
+; history = []]
+} in
+{
+players = [player1; player2; player3;player4]
+; rules = initial_rules
+; deck = []
+; discard_pile = []
+; table = initial_table_state
+; phase = Dealing
+; decision = In_progress { whose_turn = 0 ; starting_player = None}
 }
 
