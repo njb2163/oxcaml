a6f7908c6112737c0ed5f03187d7012ee607e516
Nicholas Bindela <157432778+njb2163@users.noreply.github.com>
2025-11-04 17:24:01 +0000

    add lobby

--- ui/presidents_ui.ml
+++ ui/presidents_ui.ml
@@ -5,24 +5,139 @@ open Virtual_dom
 open! Bonsai.Let_syntax
 open Js_of_ocaml
 
+(* Lobby screen state *)
+module Lobby_screen = struct
+  type t =
+    | Main_menu
+    | Creating_lobby
+    | Joining_lobby
+    | In_lobby of
+        { game_id : string
+        ; players : string list
+        ; is_host : bool
+        }
+    | Playing
+  [@@deriving sexp, equal]
+end
 
-let create_game_in_firestore () =
+
+(* Generate a short, readable game ID *)
+let generate_game_id () =
+  let timestamp = int_of_float (Js.to_float (new%js Js.date_now)##getTime) in
+  let random_suffix = Random.int 9999 in
+  Printf.sprintf "%d-%d" timestamp random_suffix
+;;
+
+(* Create a new lobby in Firebase *)
+let create_lobby ~set_lobby_screen ~set_error_message =
+  let game_id = generate_game_id () in
   let xhr = XmlHttpRequest.create () in
   let url =
-    "https://firestore.googleapis.com/v1/projects/presidents-game/databases/(default)/documents/game?key=AIzaSyAhgME9mU9-4G4vKi-5nuZBHt4Xur96XMw"
+    Printf.sprintf
+      "https://firestore.googleapis.com/v1/projects/presidents-game/databases/(default)/documents/lobbies?documentId=%s&key=AIzaSyAhgME9mU9-4G4vKi-5nuZBHt4Xur96XMw"
+      game_id
   in
   xhr##_open (Js.string "POST") (Js.string url) Js._true;
   xhr##setRequestHeader (Js.string "Content-Type") (Js.string "application/json");
-  let game_id = Printf.sprintf "game_%f_%d" 
-    (Js.to_float (new%js Js.date_now)##getTime)
-    (Random.int 100)
+  let body_json =
+    Printf.sprintf
+      {|{"fields":{
+        "game_id":{"stringValue":"%s"},
+        "players":{"arrayValue":{"values":[{"stringValue":"Player 1"}]}},
+        "status":{"stringValue":"waiting"},
+        "host":{"stringValue":"Player 1"}
+      }}|}
+      game_id
   in
-  let body_json = 
-    Printf.sprintf {|{"fields":{"game_id":{"stringValue":"%s"}}}|} game_id
-  in
-  let body = Js.string body_json in
-  ignore (xhr##send (Js.Opt.return body));
+  xhr##.onreadystatechange
+  := Js.wrap_callback (fun _ ->
+       match xhr##.readyState with
+       | XmlHttpRequest.DONE ->
+         let status = xhr##.status in
+         if status >= 200 && status < 300
+         then
+           ignore
+             (set_lobby_screen
+                (Lobby_screen.In_lobby
+                   { game_id; players = [ "Player 1" ]; is_host = true }))
+         else
+           ignore
+             (Vdom.Effect.Many
+                [ set_lobby_screen Lobby_screen.Main_menu
+                ; set_error_message
+                    (Some (Printf.sprintf "Failed to create lobby: %d" status))
+                ])
+       | _ -> ());
+  ignore (xhr##send (Js.Opt.return (Js.string body_json)))
 ;;
+
+(* Join an existing lobby *)
+let join_lobby
+      ~game_id
+      ~player_name
+      ~set_lobby_screen
+      ~set_error_message
+  =
+  let xhr = XmlHttpRequest.create () in
+  (* First, fetch the current lobby to get existing players *)
+  let url =
+    Printf.sprintf
+      "https://firestore.googleapis.com/v1/projects/presidents-game/databases/(default)/documents/lobbies/%s?key=AIzaSyAhgME9mU9-4G4vKi-5nuZBHt4Xur96XMw"
+      game_id
+  in
+  xhr##_open (Js.string "GET") (Js.string url) Js._true;
+  xhr##.onreadystatechange
+  := Js.wrap_callback (fun _ ->
+       match xhr##.readyState with
+       | XmlHttpRequest.DONE ->
+         let status = xhr##.status in
+         if status >= 200 && status < 300
+         then
+           (* Parse response and add player (simplified - you'd need proper JSON parsing) *)
+           ignore (Vdom.Effect.Many
+                            [ set_lobby_screen
+                                (Lobby_screen.In_lobby
+                                   { game_id = game_id
+                                   ;  players = [ "Player 1"; player_name ]
+                                   ; is_host = false
+                                   })
+                            ; set_error_message None
+                            ])
+         else if status = 404
+         then ignore( set_error_message (Some "Lobby not found"))
+         else ignore (set_error_message (Some (Printf.sprintf "Failed to join: %d" status)))
+       | _ -> ());
+  ignore (xhr##send Js.null)
+;;
+
+
+(* Start the game (host only) *)
+let start_game ~game_id ~game_state ~set_game_state ~set_lobby_screen ~set_error_message =
+  let xhr = XmlHttpRequest.create () in
+  let url =
+    Printf.sprintf
+      "https://firestore.googleapis.com/v1/projects/presidents-game/databases/(default)/documents/lobbies/%s?updateMask.fieldPaths=status&key=AIzaSyAhgME9mU9-4G4vKi-5nuZBHt4Xur96XMw"
+      game_id
+  in
+  xhr##_open (Js.string "PATCH") (Js.string url) Js._true;
+  xhr##setRequestHeader (Js.string "Content-Type") (Js.string "application/json");
+  let body_json = {|{"fields":{"status":{"stringValue":"started"}}}|} in
+  xhr##.onreadystatechange
+  := Js.wrap_callback (fun _ ->
+       match xhr##.readyState with
+       | XmlHttpRequest.DONE ->
+         let status = xhr##.status in
+         if status >= 200 && status < 300
+         then (
+           let new_state = Game_State.deal_cards game_state in
+           ignore
+             (Vdom.Effect.Many
+                [ set_game_state new_state; set_lobby_screen Lobby_screen.Playing ]))
+         else ignore (set_error_message (Some "Failed to start game"))
+       | _ -> ());
+  ignore (xhr##send (Js.Opt.return (Js.string body_json)))
+;;
+
 
 let presidents_board
       ~(game_state : Game_State.t)
@@ -31,7 +146,172 @@ let presidents_board
       ~set_selected_cards
       ~(error_message : string option)
       ~set_error_message
+      ~(lobby_screen : Lobby_screen.t)
+      ~set_lobby_screen
+      ~(join_game_id_input : string)
+      ~set_join_game_id_input
   =
+  (* ========== LOBBY SCREENS ========== *)
+
+  (* Render main menu *)
+  let render_main_menu () =
+    Vdom.Node.div
+      ~attrs:[ Vdom.Attr.class_ "main-menu" ]
+      [ Vdom.Node.h1 [ Vdom.Node.text "Presidents Card Game" ]
+      ; Vdom.Node.button
+          ~attrs:
+            [ Vdom.Attr.class_ "menu-button create-button"
+            ; Vdom.Attr.on_click (fun _ ->
+                (* Trigger async operation but ignore the immediate effect *)
+                create_lobby ~set_lobby_screen ~set_error_message;
+                set_lobby_screen Lobby_screen.Creating_lobby)
+            ]
+          [ Vdom.Node.text "Create Lobby" ]
+      ; Vdom.Node.button
+          ~attrs:
+            [ Vdom.Attr.class_ "menu-button join-button"
+            ; Vdom.Attr.on_click (fun _ -> set_lobby_screen Lobby_screen.Joining_lobby)
+            ]
+          [ Vdom.Node.text "Join Lobby" ]
+      ; (match error_message with
+         | None -> Vdom.Node.none
+         | Some msg ->
+           Vdom.Node.div
+             ~attrs:[ Vdom.Attr.class_ "error-message" ]
+             [ Vdom.Node.text msg ])
+      ]
+  in
+  (* Render join lobby screen *)
+  let render_join_lobby () =
+    Vdom.Node.div
+      ~attrs:[ Vdom.Attr.class_ "join-lobby-screen" ]
+      [ Vdom.Node.h2 [ Vdom.Node.text "Join Lobby" ]
+      ; Vdom.Node.div
+          ~attrs:[ Vdom.Attr.class_ "input-container" ]
+          [ Vdom.Node.label [ Vdom.Node.text "Game ID:" ]
+          ; Vdom.Node.input
+              ~attrs:
+                [ Vdom.Attr.class_ "game-id-input"
+                ; Vdom.Attr.type_ "text"
+                ; Vdom.Attr.placeholder "Enter Game ID (e.g., GAME1234)"
+                ; Vdom.Attr.value join_game_id_input
+                ; Vdom.Attr.on_input (fun _ input -> set_join_game_id_input input)
+                ]
+              ()
+          ]
+      ; Vdom.Node.div
+          ~attrs:[ Vdom.Attr.class_ "button-group" ]
+          [ Vdom.Node.button
+              ~attrs:
+                [ Vdom.Attr.class_ "action-button join-confirm-button"
+                ; Vdom.Attr.on_click (fun _ ->
+                    if String.is_empty join_game_id_input
+                    then set_error_message (Some "Please enter a Game ID")
+                    else(
+                      join_lobby
+                        ~game_id:join_game_id_input
+                        ~player_name:"Player 2"
+                        ~set_lobby_screen
+                        ~set_error_message
+                        ;
+                    set_lobby_screen Lobby_screen.Joining_lobby))
+                ]
+              [ Vdom.Node.text "Join" ]
+          ; Vdom.Node.button
+              ~attrs:
+                [ Vdom.Attr.class_ "action-button back-button"
+                ; Vdom.Attr.on_click (fun _ ->
+                    Vdom.Effect.Many
+                      [ set_lobby_screen Lobby_screen.Main_menu
+                      ; set_error_message None
+                      ; set_join_game_id_input ""
+                      ])
+                ]
+              [ Vdom.Node.text "Back" ]
+          ]
+      ; (match error_message with
+         | None -> Vdom.Node.none
+         | Some msg ->
+           Vdom.Node.div
+             ~attrs:[ Vdom.Attr.class_ "error-message" ]
+             [ Vdom.Node.text msg ])
+      ]
+  in
+  (* Render lobby waiting screen *)
+  let render_in_lobby ~game_id ~players ~is_host =
+    Vdom.Node.div
+      ~attrs:[ Vdom.Attr.class_ "lobby-waiting-screen" ]
+      [ Vdom.Node.h2 [ Vdom.Node.text "Game Lobby" ]
+      ; Vdom.Node.div
+          ~attrs:[ Vdom.Attr.class_ "game-id-display" ]
+          [ Vdom.Node.strong [ Vdom.Node.text "Game ID: " ]
+          ; Vdom.Node.span
+              ~attrs:[ Vdom.Attr.class_ "game-id-value" ]
+              [ Vdom.Node.text game_id ]
+          ]
+      ; Vdom.Node.div
+          ~attrs:[ Vdom.Attr.class_ "players-section" ]
+          [ Vdom.Node.h3 [ Vdom.Node.text "Players:" ]
+          ; Vdom.Node.ul
+              ~attrs:[ Vdom.Attr.class_ "players-list" ]
+              (List.mapi players ~f:(fun i player ->
+                 Vdom.Node.li
+                   ~attrs:[ Vdom.Attr.class_ "player-item" ]
+                   [ Vdom.Node.text
+                       (Printf.sprintf
+                          "%s%s"
+                          player
+                          (if i = 0 && is_host then " (Host)" else ""))
+                   ]))
+          ; Vdom.Node.div
+              ~attrs:[ Vdom.Attr.class_ "player-count" ]
+              [ Vdom.Node.text
+                  (Printf.sprintf "Waiting for players... (%d/4)" (List.length players))
+              ]
+          ]
+      ; Vdom.Node.div
+          ~attrs:[ Vdom.Attr.class_ "lobby-actions" ]
+          ((if is_host
+            then
+              [ Vdom.Node.button
+                  ~attrs:
+                    [ Vdom.Attr.class_ "action-button start-game-button"
+                    ; Vdom.Attr.on_click (fun _ ->
+                        start_game
+                          ~game_id
+                          ~game_state
+                          ~set_game_state
+                          ~set_lobby_screen
+                          ~set_error_message;
+                          Vdom.Effect.Ignore)
+                    ]
+                  [ Vdom.Node.text "Start Game" ]
+              ]
+            else
+              [ Vdom.Node.div
+                  ~attrs:[ Vdom.Attr.class_ "waiting-message" ]
+                  [ Vdom.Node.text "Waiting for host to start the game..." ]
+              ])
+           @ [ Vdom.Node.button
+                 ~attrs:
+                   [ Vdom.Attr.class_ "action-button leave-button"
+                   ; Vdom.Attr.on_click (fun _ ->
+                       Vdom.Effect.Many
+                         [ set_lobby_screen Lobby_screen.Main_menu
+                         ; set_error_message None
+                         ])
+                   ]
+                 [ Vdom.Node.text "Leave Lobby" ]
+             ])
+      ; (match error_message with
+         | None -> Vdom.Node.none
+         | Some msg ->
+           Vdom.Node.div
+             ~attrs:[ Vdom.Attr.class_ "error-message" ]
+             [ Vdom.Node.text msg ])
+      ]
+  in
+  (* ========== GAME RENDERING (existing code) ========== *)
   let render_deal_button ~(game_state : Game_State.t) ~set_game_state =
     Vdom.Node.button
       ~attrs:
@@ -41,28 +321,6 @@ let presidents_board
             set_game_state new_state)
         ]
       [ Vdom.Node.text "Deal Cards" ]
-  in
-  let render_create_lobby_button ~(game_state : Game_State.t) ~set_game_state =
-  Vdom.Node.button
-    ~attrs:
-      [ Vdom.Attr.class_ "deal-button"
-      ; Vdom.Attr.on_click (fun _ ->
-          create_game_in_firestore ();
-          let new_state = Game_State.deal_cards game_state in
-          set_game_state new_state)
-      ]
-    [ Vdom.Node.text "Create Lobby" ]
-  in
-    let render_join_lobby_button ~(game_state : Game_State.t) ~set_game_state =
-  Vdom.Node.button
-    ~attrs:
-      [ Vdom.Attr.class_ "deal-button"
-      ; Vdom.Attr.on_click (fun _ ->
-          create_game_in_firestore ();
-          let new_state = Game_State.deal_cards game_state in
-          set_game_state new_state)
-      ]
-    [ Vdom.Node.text "Join Lobby" ]
   in
   let render_trick ~current_trick =
     Vdom.Node.div
@@ -151,58 +409,69 @@ let presidents_board
              ]
          ]))
   in
-  Vdom.Node.div
-    ~attrs:[ Vdom.Attr.class_ "game" ]
-    (match game_state.phase with
-     | Phase.Dealing -> [ render_deal_button ~game_state ~set_game_state ; render_create_lobby_button ~game_state ~set_game_state ; render_join_lobby_button ~game_state ~set_game_state ]
-     | Phase.DeckPicking ->
-       [ Vdom.Node.div
-           ~attrs:[ Vdom.Attr.class_ "deck-picking" ]
-           [ Vdom.Node.text "Deck Picking Phase - Not Implemented Yet" ]
-       ]
-     | Phase.Playing ->
-       let trick_node =
-         render_trick ~current_trick:(Table_State.cards_in_trick game_state.table)
-       in
-       let current_player_idx =
-         match game_state.decision with
-         | In_progress { whose_turn } -> Some whose_turn
-         | _ -> None
-       in
-       let player_nodes =
-         List.map game_state.players ~f:(fun player ->
-           render_hand ~player ~current_player_idx)
-       in
-       let action_button = render_action_button ~selected_cards ~current_player_idx in
-       let error_display = render_error_message ~error_message in
-       [ trick_node; action_button; error_display ] @ player_nodes
-     | Phase.RoundEnd ->
-       (match game_state.decision with
-        | Round_Over { round_ranking } ->
-          let ranking_nodes =
-            List.mapi round_ranking ~f:(fun i player_idx ->
-              let player = Player.lookup_player_exn game_state.players player_idx in
-              Vdom.Node.div
-                ~attrs:[ Vdom.Attr.class_ "ranking-entry" ]
-                [ Vdom.Node.text (Printf.sprintf "%d. %s" (i + 1) player.name) ])
-          in
-          [ Vdom.Node.div
-              ~attrs:[ Vdom.Attr.class_ "round-over" ]
-              ([ Vdom.Node.div
-                   ~attrs:[ Vdom.Attr.class_ "round-over-title" ]
-                   [ Vdom.Node.text "Round Over!" ]
-               ]
-               @ ranking_nodes)
-          ]
-        | _ ->
-          [ Vdom.Node.div
-              ~attrs:[ Vdom.Attr.class_ "round-over" ]
-              [ Vdom.Node.text "Round Over!" ]
-          ]))
+  let render_game_screen () =
+    Vdom.Node.div
+      ~attrs:[ Vdom.Attr.class_ "game" ]
+      (match game_state.phase with
+       | Phase.Dealing -> [ render_deal_button ~game_state ~set_game_state ]
+       | Phase.DeckPicking ->
+         [ Vdom.Node.div
+             ~attrs:[ Vdom.Attr.class_ "deck-picking" ]
+             [ Vdom.Node.text "Deck Picking Phase - Not Implemented Yet" ]
+         ]
+       | Phase.Playing ->
+         let trick_node =
+           render_trick ~current_trick:(Table_State.cards_in_trick game_state.table)
+         in
+         let current_player_idx =
+           match game_state.decision with
+           | In_progress { whose_turn } -> Some whose_turn
+           | _ -> None
+         in
+         let player_nodes =
+           List.map game_state.players ~f:(fun player ->
+             render_hand ~player ~current_player_idx)
+         in
+         let action_button = render_action_button ~selected_cards ~current_player_idx in
+         let error_display = render_error_message ~error_message in
+         [ trick_node; action_button; error_display ] @ player_nodes
+       | Phase.RoundEnd ->
+         (match game_state.decision with
+          | Round_Over { round_ranking } ->
+            let ranking_nodes =
+              List.mapi round_ranking ~f:(fun i player_idx ->
+                let player = Player.lookup_player_exn game_state.players player_idx in
+                Vdom.Node.div
+                  ~attrs:[ Vdom.Attr.class_ "ranking-entry" ]
+                  [ Vdom.Node.text (Printf.sprintf "%d. %s" (i + 1) player.name) ])
+            in
+            [ Vdom.Node.div
+                ~attrs:[ Vdom.Attr.class_ "round-over" ]
+                ([ Vdom.Node.div
+                     ~attrs:[ Vdom.Attr.class_ "round-over-title" ]
+                     [ Vdom.Node.text "Round Over!" ]
+                 ]
+                 @ ranking_nodes)
+            ]
+          | _ ->
+            [ Vdom.Node.div
+                ~attrs:[ Vdom.Attr.class_ "round-over" ]
+                [ Vdom.Node.text "Round Over!" ]
+            ]))
+  in
+  (* ========== MAIN ROUTER ========== *)
+  match lobby_screen with
+  | Main_menu -> render_main_menu ()
+  | Joining_lobby -> render_join_lobby ()
+  | Creating_lobby ->
+    Vdom.Node.div
+      ~attrs:[ Vdom.Attr.class_ "loading" ]
+      [ Vdom.Node.text "Creating lobby..." ]
+  | In_lobby { game_id; players; is_host } -> render_in_lobby ~game_id ~players ~is_host
+  | Playing -> render_game_screen ()
 ;;
 
 let app =
-  (* Set initial state *)
   let initial_state =
     Game_State.create
       ~players:4
@@ -213,7 +482,6 @@ let app =
   let%sub game_state, set_game_state =
     Bonsai.state ~default_model:initial_state (module Game_State)
   in
-  (* Add state for selected cards *)
   let%sub selected_cards, set_selected_cards =
     Bonsai.state
       ~default_model:[]
@@ -221,7 +489,6 @@ let app =
         type t = Card.t list [@@deriving sexp, equal]
       end)
   in
-  (* Add state for error message *)
   let%sub error_message, set_error_message =
     Bonsai.state
       ~default_model:None
@@ -229,12 +496,28 @@ let app =
         type t = string option [@@deriving sexp, equal]
       end)
   in
+  (* Add lobby screen state *)
+  let%sub lobby_screen, set_lobby_screen =
+    Bonsai.state ~default_model:Lobby_screen.Main_menu (module Lobby_screen)
+  in
+  (* Add join game ID input state *)
+  let%sub join_game_id_input, set_join_game_id_input =
+    Bonsai.state
+      ~default_model:""
+      (module struct
+        type t = string [@@deriving sexp, equal]
+      end)
+  in
   let%arr game_state = game_state
   and set_game_state = set_game_state
   and selected_cards = selected_cards
   and set_selected_cards = set_selected_cards
   and error_message = error_message
-  and set_error_message = set_error_message in
+  and set_error_message = set_error_message
+  and lobby_screen = lobby_screen
+  and set_lobby_screen = set_lobby_screen
+  and join_game_id_input = join_game_id_input
+  and set_join_game_id_input = set_join_game_id_input in
   presidents_board
     ~game_state
     ~set_game_state
@@ -242,6 +525,10 @@ let app =
     ~set_selected_cards
     ~error_message
     ~set_error_message
+    ~lobby_screen
+    ~set_lobby_screen
+    ~join_game_id_input
+    ~set_join_game_id_input
 ;;
 
 let () = Bonsai_web.Start.start app
